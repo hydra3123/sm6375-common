c7c4e598b42591aecf7de9e2f1958f1a567eabf6
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-02-08 02:30:49 +0000

    Revert "sm6375-common: Switch to common QTI vibrator HAL"
    This reverts commit 8022cec53aac939f03be7f22c4280c7c7c597428.

--- common.mk
+++ common.mk
@@ -421,7 +421,8 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
