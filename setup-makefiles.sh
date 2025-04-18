19c199648f8ef76e999eb71aeee1669f7df2cc3c
hydra3123 <harshit18461846@gmail.com>
2025-04-18 04:32:09 +0000

    Revert "sm6375-common: fix com.qualcomm.qti.dpm.api@1.0 ELF checks"
    This reverts commit ea1de6d814beedc5c30b2da58ba56687e397f777.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,8 +24,48 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+function vendor_imports() {
+    cat <<EOF >>"$1"
+ 		"device/motorola/sm6375-common",
+         "hardware/motorola",
+         "hardware/qcom-caf/wlan",
+         "hardware/qcom-caf/sm8350",
+         "vendor/qcom/opensource/usb/etc",
+         "vendor/qcom/opensource/commonsys-intf/display",
+         "vendor/qcom/opensource/commonsys/display",
+         "vendor/qcom/opensource/display",
+         "vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um",
+         "vendor/qcom/opensource/dataservices",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.qualcomm.qti.dpm.api@1.0 | \
+            vendor.qti.hardware.fm@1.0)
+            echo "$1-vendor"
+            ;;
+        libqsap_sdk | \
+            libril | \
+            libwpa_client) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+        lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
 write_headers
